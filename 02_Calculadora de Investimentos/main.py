c6e69532b91c7f4ca8b89eac81a52597194c9f48
DevRafaDeSousa20 <rafaelasousa1718@gmail.com>
2025-04-24 19:09:16 -0300

    Contém as funções da Calculadora.
    Arquivo exclusivo para as funções da calculadora, propriamente dita. Este arquivo será criado a parte, para que quaisquer alterações feitas na interface, ou em qualquer outro quesito, não interrompa ou danifique a funcionalidade e a precisão dos cálculos.

---
+++ 02_Calculadora de Investimentos/main.py
@@ -0,0 +1,12 @@
+# 
+
+# Criando a função para calcular 
+def calcular_lucro(investimento_inicial, dias_de_investimento, taxa_de_risco,  retorno_garantido):
+        retorno_diario = 0
+        lucro_diario = 0
+        lucro_semanal = 0
+        lucro_mensal = 0
+        lucro_anual = 0
+        lucro_total = 0
+
+ 
